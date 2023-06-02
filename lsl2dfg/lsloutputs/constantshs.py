a0559b5a5928a277033482343d95afe9dd1ebc3c
Sasha Wyrding <firemouse22@gmail.com>
2023-06-02 18:29:41 -0400

    Define cmp function
    The cmp function doesn't exist under python 3, so replace it with a functional equivalent for the datatypes used.

--- lsl2dfg/lsloutputs/constantshs.py
+++ lsl2dfg/lsloutputs/constantshs.py
@@ -27,6 +27,10 @@
 import sys
 import re
 
+# Python3 does not have this function, so this replaces it with equivalent behavior.
+def cmp(a, b):
+  return (a > b) - (a < b)
+
 def output(document, defaultdescs, databaseversion, infilename, outfilename, lang, tag):
 
   def UpperCamelCase_TO_UNDERSCORES(arg):
